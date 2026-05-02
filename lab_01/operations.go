2c54abd0c1d1e1f073732bd1835b424c8abaae80
binu2002 <binojmadhuranga2002.04.04@gmail.com>
2026-05-02 15:53:54 +0530

    Add assignment operations to operations.go

--- lab_01/operations.go
+++ lab_01/operations.go
@@ -34,5 +34,16 @@ func main() {
     fmt.Println("x || y:", x || y)
     fmt.Println("!x:", !x)
 
-   
+    // 🔹 Assignment
+    c := 5
+    fmt.Println("\nAssignment Operations:")
+    c += 2
+    fmt.Println("c += 2:", c)
+    c -= 1
+    fmt.Println("c -= 1:", c)
+    c *= 3
+    fmt.Println("c *= 3:", c)
+    c /= 2
+    fmt.Println("c /= 2:", c)
+
 }
