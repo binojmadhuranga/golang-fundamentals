e4f9641a8aa257f123d1b951aaa56fb1b9cb38f8
binu2002 <binojmadhuranga2002.04.04@gmail.com>
2026-05-02 11:50:51 +0530

    Refactor comments for variable sections to improve readability

--- lab_01/variables.go
+++ lab_01/variables.go
@@ -6,50 +6,50 @@ import (
 
 func main() {
 
-	// 🔹 1. Integer types
+	
 	var a int = 10
 	var b int8 = 127
 	var c int16 = 32000
 	var d int32 = 2000000000
 	var e int64 = 9000000000000
 
-	// 🔹 2. Unsigned integers
+	
 	var u uint = 20
 	var u8 uint8 = 255
 	var u16 uint16 = 65000
 	var u32 uint32 = 4000000000
 	var u64 uint64 = 18000000000000000000
 
-	// 🔹 3. Floating point
+	
 	var f1 float32 = 3.14
 	var f2 float64 = 3.1415926535
 
-	// 🔹 4. Boolean
+	
 	var isGoAwesome bool = true
 
-	// 🔹 5. String
+	
 	var name string = "Binoj"
 
-	// 🔹 6. Short declaration (type inference)
+	
 	x := 100
 	y := "Hello Go"
 
-	// 🔹 7. Array
+
 	var numbers [3]int = [3]int{1, 2, 3}
 
-	// 🔹 8. Slice (dynamic array)
+	
 	slice := []int{10, 20, 30}
 
-	// 🔹 9. Map (key-value)
+	
 	student := map[string]int{
 		"math":    90,
 		"science": 85,
 	}
 
-	// 🔹 10. Pointer
+	
 	p := &a
 
-	// 🔹 11. Struct
+	
 	type Person struct {
 		name string
 		age  int
