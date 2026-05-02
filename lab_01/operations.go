e3e3fd22f3dd1add68e43b0ca392e6152afc327a
binu2002 <binojmadhuranga2002.04.04@gmail.com>
2026-05-02 15:52:46 +0530

    Add operations.go for basic arithmetic operations and variable declarations

---
+++ lab_01/operations.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+
+    // Variables
+    a := 10
+    b := 3
+
+    // 🔹 Arithmetic
+    fmt.Println("Arithmetic Operations:")
+    fmt.Println("Addition:", a+b)
+    fmt.Println("Subtraction:", a-b)
+    fmt.Println("Multiplication:", a*b)
+    fmt.Println("Division:", a/b)
+    fmt.Println("Modulus:", a%b)
+
+   
+}
